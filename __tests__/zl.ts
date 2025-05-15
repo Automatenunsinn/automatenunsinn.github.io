42b11c35ff4e9f1690da01fdcd153bc4ce92abcb
stonedDiscord <Tukz@gmx.de>
2025-05-15 12:38:54 +0000

    format test

--- __tests__/zl.ts
+++ __tests__/zl.ts
@@ -1,23 +1,5 @@
 import { convertZlDataToArray, generateZlCodeV2, generateZlCodeV3, rotateZlStr } from '../src/zl';
 
-describe('rotateZlStr', () => {
-    it('rotata zl str', () => {
-        const zlStr = [4, 0, 0, 1, 1, 2, 3, 4, 5, 1, 0, 2, 4];
-        const firstPart = new Array(5).fill('0');
-        const expectedZlStr = [0,0,1,1,2,3,4,5,1,0,2,4,4];
-        const expectedFirstPart = [
-            String.fromCharCode(1 + 0x30),
-            String.fromCharCode(0 + 0x30),
-            String.fromCharCode(2 + 0x30),
-            String.fromCharCode(4 + 0x30),
-            '0'
-        ];
-        rotateZlStr(zlStr, firstPart);
-        expect(zlStr).toEqual(expectedZlStr);
-        expect(firstPart).toEqual(expectedFirstPart);
-    });
-});
-
 test('array conversion', () => {
     const zlData = { zlNr: 400112345, code: 10240000 };
     const zlnr = new Array(13).fill(0);
@@ -29,8 +11,24 @@ test('array conversion', () => {
     ]);
 });
 
+test('rotation', () => {
+    const zlStr = [4, 0, 0, 1, 1, 2, 3, 4, 5, 1, 0, 2, 4];
+    const firstPart = new Array(5).fill('0');
+    const expectedZlStr = [0, 0, 1, 1, 2, 3, 4, 5, 1, 0, 2, 4, 4];
+    const expectedFirstPart = [
+        String.fromCharCode(1 + 0x30),
+        String.fromCharCode(0 + 0x30),
+        String.fromCharCode(2 + 0x30),
+        String.fromCharCode(4 + 0x30),
+        '0'
+    ];
+    rotateZlStr(zlStr, firstPart);
+    expect(zlStr).toEqual(expectedZlStr);
+    expect(firstPart).toEqual(expectedFirstPart);
+});
+
 test('code v2', () => {
-    const data = [0,0,1,1,2,3,4,5,1,0,2,4,4];
+    const data = [0, 0, 1, 1, 2, 3, 4, 5, 1, 0, 2, 4, 4];
     const result = generateZlCodeV2([...data]);
     expect(result).toBe(2050);
     expect(result).toBeGreaterThanOrEqual(0);
@@ -38,7 +36,7 @@ test('code v2', () => {
 });
 
 test('code v3', () => {
-    const data = [0,0,1,1,2,3,4,5,1,0,2,4,4];
+    const data = [0, 0, 1, 1, 2, 3, 4, 5, 1, 0, 2, 4, 4];
     const result = generateZlCodeV3([...data]);
     expect(result).toBe(871);
     expect(result).toBeGreaterThanOrEqual(0);
