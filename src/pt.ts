c216e6af55207a982dec4d54cdc6a06b378b8ee4
stonedDiscord <Tukz@gmx.de>
2025-06-15 21:44:45 +0000

    volser

--- src/pt.ts
+++ src/pt.ts
@@ -2,28 +2,38 @@ import abCheck from './abCheck';
 
 declare global {
     interface Window {
+        calcRequestCode: () => void;
         calculateCode: () => void;
         downloadCode: () => void;
     }
 }
 
+export function calcRequestCode() {
+    const volSer = document.getElementById('volumeSerial') as HTMLInputElement;
+    const reqCode = document.getElementById('requestCode') as HTMLInputElement;
+    const volNum = parseInt(volSer.value.replace("-",""), 16);
+    const plusVolNum = volNum + 0x0F9DEE1;
+    const hexCode = (plusVolNum & 0x7FFFFFFF);
+    reqCode.value = hexCode.toString();
+}
+
 export default function calculateCode() {
-    const maskEdit1 = document.getElementById('maskEditInput') as HTMLInputElement;
-    const label1 = document.getElementById('out') as HTMLInputElement;
+    const reqCode = document.getElementById('requestCode') as HTMLInputElement;
+    const outLabel = document.getElementById('out') as HTMLInputElement;
     const dlbtn = document.getElementById('downloadButton') as HTMLInputElement;
 
     let lvar_4: string;
     let lvar_8: string;
 
     try {
-        lvar_4 = maskEdit1.value;
+        lvar_4 = reqCode.value;
         let EBX = parseInt(lvar_4, 10);
         
         if (isNaN(EBX)) {
             throw new Error('falscher Abruf Code!');
         }
 
-        maskEdit1.style.backgroundColor = "#353";
+        reqCode.style.backgroundColor = "#353";
 
         EBX &= 2147483647; // EBX And $7FFFFFFF{2147483647}
         const ECX = EBX & 15; // EBX And 15
@@ -33,19 +43,19 @@ export default function calculateCode() {
         EBX &= 2147483647; // (EBX) And $7FFFFFFF{2147483647}
 
         lvar_8 = (EBX & 2147483647).toString(); // (EBX) And $7FFFFFFF{2147483647}{EAX}
-        label1.value = lvar_8;
-        label1.style.animation = "shine 1s ease-in infinite";
+        outLabel.value = lvar_8;
+        outLabel.style.animation = "shine 1s ease-in infinite";
         dlbtn.disabled = false;
 
     } catch (error: unknown) {
-        maskEdit1.style.backgroundColor = "#533";
+        reqCode.style.backgroundColor = "#533";
         dlbtn.disabled = true;
     }
 }
 
 export function downloadCode() {
-    const label1 = document.getElementById('out') as HTMLInputElement;
-    const lvar_8 = label1.textContent;
+    const outLabel = document.getElementById('out') as HTMLInputElement;
+    const lvar_8 = outLabel.textContent;
 
     const regContent = `Windows Registry Editor Version 5.00
 
@@ -65,5 +75,6 @@ export function downloadCode() {
     window.URL.revokeObjectURL(url);
 }
 
+window.calcRequestCode = calcRequestCode;
 window.calculateCode = calculateCode;
 window.downloadCode = downloadCode;
