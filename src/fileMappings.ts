1daaa99013271328a9d950d202faa93cb60bc498
pcbanalyst <pcbanalyst@protonmail.com>
2026-02-25 14:48:23 +0100

    first map

---
+++ src/fileMappings.ts
@@ -0,0 +1,26 @@
+export const roteDBFiles = [
+  "ALSUNA_EC1.bin"
+]
+
+export const blaugelb1MBDBFiles = [
+  "ALEX_CC4.bin"
+];
+
+export const blaugelbUHGDBFiles = [
+  "ASTERIX_UND_LATRAVIATA_C3.1.bin"
+];
+
+export const gelbeDBFiles = [
+  "ALEX_CC5.bin"
+];
+
+export const lila2MBFiles = [
+  "Money_X_CC2.bin"
+];
+
+export const factoryResetFiles = [
+  { name: "Factory Reset 2MB", bin: "Factory_2MB.bin", loader: "lila2MB" },
+  { name: "Factory Reset 3.0 alte 1MB", bin: "Factory_3.0_alte_1mb.bin", loader: "blaugelb1MB" },
+  { name: "Factory Reset 2.0", bin: "FactoryReset20.Xc", loader: "roteDB" },
+  { name: "Factory Reset 5.0a 1MB (512KB images)", bin: "Factory_5.0a_1MB_Redesing_fur_512KB_images.bin", loader: "blaugelb1MB" }
+]
